51006340832156f4c3bd207390f59735cc8c70e7
garima <garima.sanghai@gmail.com>
2024-12-11 07:53:39 +0530

    New API code changes

--- src/main/java/org/APIAutomationRestAssured/Sample01.java
+++ src/main/java/org/APIAutomationRestAssured/Sample01.java
@@ -2,6 +2,6 @@ package org.APIAutomationRestAssured;
 
 public class Sample01 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World..");
     }
 }

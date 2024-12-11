63dfa9d3d0ea39090e6f95c8bce84323ff533907
garima <garima.sanghai@gmail.com>
2024-12-11 20:51:54 +0530

    New API code changes

--- src/test/java/org/APIAutomationRestAssured/samplecheck01/Task03.java
+++ src/test/java/org/APIAutomationRestAssured/samplecheck01/Task03.java
@@ -2,26 +2,26 @@ package org.APIAutomationRestAssured.samplecheck01;
 
 import io.restassured.RestAssured;
 
-public class APITesting01 {
+public class Task03 {
     public static void main(String[] args) {
-        // Gherkins Syntax
         // Given() -> Pre Req. - URL, Headers, Auth, Body....
         // When() -> HTTP method? - GET/POST/PUT/PATCH, DELETE...
-        // Then() -> Validation -> 200 oK, firstname == PRAMOD
+        // Then() -> Validation -> 200 oK, firstname == Garima
 
-
-        // Full URL - https://api.zippopotam.us/IN/560016
+        // Full URL - https://api.zippopotam.us/IN/734005
         // base URI - https://api.zippopotam.us
         // bath Path - /IN/560016
+
         RestAssured
                 .given()
-                 .baseUri("https://api.zippopotam.us")
-                 .basePath("/IN/560016")
+                    .baseUri("https://api.zippopotam.us")
+                    .basePath("/IN/560016")
                 .when()
-                 .get() //http method
+                    .get()
                 .then()
-                 .log().all().statusCode(200);
+                    .log().all().statusCode(200);
 
         // Expected status code <404> but was <200>
+
     }
 }
